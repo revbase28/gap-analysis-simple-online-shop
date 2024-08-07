f0dec17a5a35b83405ff1ad0f4389d50a5f29508
revbase28 <reihanreinaldimctna@gmail.com>
2024-08-07 11:21:39 +0700

    Init

---
+++ be-simple-online-shop/src/main/java/com/gap/analysis/be_simple_online_shop/BeSimpleOnlineShopApplication.java
@@ -0,0 +1,13 @@
+package com.gap.analysis.be_simple_online_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeSimpleOnlineShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeSimpleOnlineShopApplication.class, args);
+	}
+
+}
---
+++ be-simple-online-shop/src/test/java/com/gap/analysis/be_simple_online_shop/BeSimpleOnlineShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gap.analysis.be_simple_online_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeSimpleOnlineShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
